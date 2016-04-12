2008350ef654a909a7e1a38f953c5f50b24e0f92
iAPT <abisheksamuel09@gmail.com>
2016-04-12 20:08:29 +0530

    master commit 1
    master commit 1

---
+++ appointment/application/logs/log-2016-04-09.php
@@ -0,0 +1,18 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-04-09 09:32:07 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-09 09:32:07 --> Unable to connect to the database
+ERROR - 2016-04-09 09:32:28 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-09 09:32:28 --> Unable to connect to the database
+ERROR - 2016-04-09 09:46:42 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-09 09:46:42 --> Unable to connect to the database
+ERROR - 2016-04-09 10:54:12 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-09 10:54:12 --> Unable to connect to the database
+ERROR - 2016-04-09 11:02:40 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-09 11:02:40 --> Unable to connect to the database
+ERROR - 2016-04-09 13:58:39 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-09 13:58:39 --> Unable to connect to the database
+ERROR - 2016-04-09 18:04:48 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-09 18:04:48 --> Unable to connect to the database
+ERROR - 2016-04-09 18:08:14 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-09 18:08:14 --> Unable to connect to the database
---
+++ appointment/application/logs/log-2016-04-10.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2016-04-10 02:38:11 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-10 02:38:11 --> Unable to connect to the database
+ERROR - 2016-04-10 09:35:16 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-10 09:35:16 --> Unable to connect to the database
+ERROR - 2016-04-10 09:35:34 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-10 09:35:34 --> Unable to connect to the database
+ERROR - 2016-04-10 13:58:58 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-10 13:58:58 --> Unable to connect to the database
+ERROR - 2016-04-10 20:36:40 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-10 20:36:40 --> Unable to connect to the database
+ERROR - 2016-04-10 22:38:51 --> Severity: Warning  --> mysqli_connect(): (28000/1045): Access denied for user 'root'@'localhost' (using password: NO) /home/iapt4u/public_html/appointment/system/database/drivers/mysqli/mysqli_driver.php 77
+ERROR - 2016-04-10 22:38:51 --> Unable to connect to the database
